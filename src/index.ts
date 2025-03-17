94103542407e834e3f108a62a86ce418b061eb39
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2025-03-17 18:28:56 +0100

    reference & json data updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import '../dist/index.css'; // Import the Tailwind CSS styles
 export { default as Example } from './components/example/example';
 export { default as GLBViewer } from './components/glb/GLBViewer';
